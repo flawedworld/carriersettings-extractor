fcf8fad6bf310ba4802b17e796d5fc0acf7869b4
anupritaisno1 <www.anuprita804@gmail.com>
2020-09-26 06:37:18 +0000

    Revert "replace wget with curl"
    This reverts commit 9d664f28d1ab3129edff16fc7afcad62377363d3.

--- download_carrier_list.sh
+++ download_carrier_list.sh
@@ -2,6 +2,6 @@
 
 set -euo pipefail
 
-curl -fS \
+wget -qO- \
   'https://android.googlesource.com/platform/packages/providers/TelephonyProvider/+/master/assets/latest_carrier_id/carrier_list.pb?format=TEXT' | \
   base64 --decode > carrier_list.pb
